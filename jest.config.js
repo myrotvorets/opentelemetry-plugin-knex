0fd13321ba1e41b278949cc09199a3d8205be1a2
Myrotvorets <support@myrotvorets.center>
2021-04-12 16:16:38 +0300

    Update workflows

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const merge = require('merge');
 const ts_preset = require('ts-jest/jest-preset');
 
 module.exports = merge.recursive(ts_preset, {
-    collectCoverage: true,
+    collectCoverage: process.env.COLLECT_COVERAGE !== '0',
     collectCoverageFrom: [
         'lib/**/*.ts',
     ],
@@ -12,4 +12,10 @@ module.exports = merge.recursive(ts_preset, {
         '<rootDir>/dist/',
         '<rootDir>/node_modules/',
     ],
+    testResultsProcessor: 'jest-sonar-reporter',
+    reporters: [
+        "default",
+        process.env.GITHUB_ACTIONS === 'true' ? 'jest-github-actions-reporter' : null,
+    ].filter(Boolean),
+    testLocationInResults: true
 });
